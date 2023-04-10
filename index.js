88b12af459b02883734dd867df41f522b607028c
OscarV <oovillag@gmail.com>
2023-04-10 15:38:56 -0500

    Update README.md file

--- index.js
+++ index.js
@@ -136,11 +136,11 @@ class TaskManager {
     });
   });
   
-  function addClearButtonListener(taskManager) {
-    const clearButton = document.getElementById('clear-button');
-    clearButton.addEventListener('click', () => {
-      taskManager.taskList = taskManager.taskList.filter((task) => !task.completed);
-      taskManager.saveTasks();
-      window.location.reload();
-    });
-  }
+  
+  const clearButton = document.getElementById('clear-button');
+  clearButton.addEventListener('click', () => {
+    taskManager.taskList = taskManager.taskList.filter((task) => !task.completed);
+    taskManager.saveTasks();
+    window.location.reload();
+  });
+  
